e25c9f5c7d4c07fe49990e23c0b5c69da4f32edd
Michael Warren <mwarrn@amazon.com>
2022-07-31 17:53:10 -0700

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
+    # setter method
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+  
+    # getter method
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
